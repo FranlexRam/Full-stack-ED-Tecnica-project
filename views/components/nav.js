a8241d02bab2ea3e586742929b56d8eaeec30723
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-03-22 19:31:35 -0400

    Login page

--- views/components/nav.js
+++ views/components/nav.js
@@ -72,10 +72,47 @@ const createNavSignup = () => {
     `;
 }
 
+
+const createNavLogin = () => {
+    navbar.innerHTML = `
+    <div class="max-w-7xl h-16 mx-auto flex items-center px-4 justify-between">
+        <p class="font-bold text-xl text-white">TodoApp</p>
+
+        <!-- Version movil -->
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none" viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-10 h-10 md:hidden text-white cursor-pointer p-2 hover:bg-indigo-800 rounded-lg"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+        </svg>
+            
+        <!-- Version escritorio -->
+        <div class="hidden md:flex flex-row gap-4">
+            <a href="/registro/" class="transition ease-in-out text-white font-bold hover:bg-indigo-800 py-2 px-4 rounded-lg">Registro</a>
+        </div>
+
+        <!-- Menu movil -->
+        <div class="bg-slate-900/60 fixed top-16 right-0 left-0 bottom-0 justify-center items-center flex-col gap-4 hidden animate__animated animate__backOutRight">
+            <a href="/registro/" class="transition ease-in-out text-white font-bold hover:bg-indigo-800 py-2 px-4 rounded-lg">Registro</a>
+        </div>
+
+    </div>
+    `;
+}
+
 if(window.location.pathname === '/') {
     createNavHome();
 } else if (window.location.pathname === '/registro/') {
     createNavSignup();
+} else if (window.location.pathname === '/login/') {
+    createNavLogin();
 }
 
 const navBtn = navbar.children[0].children[1];
