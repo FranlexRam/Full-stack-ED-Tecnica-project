47160e6af0e6b5b7506f72f31a135a1c5f5bb1a1
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-03-22 19:03:40 -0400

    Validations 4th part

--- views/signup/index.js
+++ views/signup/index.js
@@ -3,27 +3,56 @@ const nameInput = document.querySelector('#name-input');
 const emailInput = document.querySelector('#email-input');
 const passwordInput = document.querySelector('#password-input');
 const matchInput = document.querySelector('#match-input');
+const formBtn = document.querySelector('#form-btn');
 
 //Regex validations
 const EMAIL_VALIDATION = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 const PASSWORD_VALIDATION = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 const NAME_VALIDATION = /^[A-Z\u00d1][a-zA-Z-ÿ\u00f1\u00d1]+(\s*[A-Z\u00d1][a-zA-Z-ÿ\u00f1\u00d1\s]*)$/;
 
-// Events
+//Validations
+let nameValidation = false;
+let emailValidation = false;
+let passwordValidation = false;
+let matchValidation = false;
 
-nameInput.addEventListener('input', e => {
-    const nameValidation = NAME_VALIDATION.test(e.target.value);
+const validation = (input, regexValidation) => {
+    formBtn.disabled = nameValidation && emailValidation && passwordValidation && matchValidation ? false : true;
 
-    if (e.target.value === '') {
-        nameInput.classList.remove('outline-green-400', 'outline-2', 'outline');
-        nameInput.classList.remove('outline-red-500', 'outline-2', 'outline');
-        nameInput.classList.add('focus:outline-indigo-700');
-    } else if (nameValidation) {
-        nameInput.classList.remove('focus:outline-indigo-700');
-        nameInput.classList.add('outline-green-400', 'outline-2', 'outline');
-    } else {
-        nameInput.classList.remove('focus:outline-indigo-700');
-        nameInput.classList.remove('outline-green-400', 'outline-2', 'outline');
-        nameInput.classList.add('outline-red-500', 'outline-2', 'outline');
+    if (input.value === '') {
+        input.classList.remove('outline-green-400', 'outline-2', 'outline');
+        input.classList.remove('outline-red-500', 'outline-2', 'outline');
+        input.classList.add('focus:outline-indigo-700');
+    } else if (regexValidation) {
+        input.classList.remove('focus:outline-indigo-700');
+        input.classList.add('outline-green-400', 'outline-2', 'outline');
+    } else if (!regexValidation) {
+        input.classList.remove('focus:outline-indigo-700');
+        input.classList.remove('outline-green-400', 'outline-2', 'outline');
+        input.classList.add('outline-red-500', 'outline-2', 'outline');
     }
+};
+
+
+// Events
+nameInput.addEventListener('input', e => {
+    nameValidation = NAME_VALIDATION.test(e.target.value);
+    validation(nameInput, nameValidation);
+});
+
+emailInput.addEventListener('input', e => {
+    emailValidation = EMAIL_VALIDATION.test(e.target.value);
+    validation(emailInput, emailValidation);
+});
+
+passwordInput.addEventListener('input', e => {
+    passwordValidation = PASSWORD_VALIDATION.test(e.target.value);
+    matchValidation = e.target.value === matchInput.value;
+    validation(passwordInput, passwordValidation);
+    validation(matchInput, matchValidation);
+});
+
+matchInput.addEventListener('input', e => {
+    matchValidation = e.target.value === passwordInput.value;
+    validation(matchInput, matchValidation);
 });
