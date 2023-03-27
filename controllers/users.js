797e22cdd3aeedddd726a0a0f292b1f81aab9249
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-03-27 18:54:55 -0400

    Backend, guardar en Mongo DB

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const usersRouter = require('express').Router();
+const User = require('../models/user.js');
+const bcrypt = require('bcrypt');
 
 usersRouter.post('/', async (request,response) =>{
     const { name, email, password } = request.body;
@@ -6,6 +8,20 @@ usersRouter.post('/', async (request,response) =>{
     if(!name || !email || !password) {
         return response.status(400).json({ error: 'Todos los espacios son requeridos' });
     }
+
+    const saltRounds = 10;
+
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+    
+    const newUser = new User({
+        name,
+        email,
+        passwordHash,
+    });
+
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+
 });
 
 module.exports = usersRouter;
